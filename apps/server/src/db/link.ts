f211dc2705c2ce419810ca9705e8eb9e6f64d8c3
thezzisu <thezzisu@gmail.com>
2023-04-13 09:02:09 +0000

    chore: update server docs & eslint

--- apps/server/src/db/link.ts
+++ apps/server/src/db/link.ts
@@ -1,10 +1,36 @@
 import { database } from './base.js'
 
+/**
+ * The Link Interface
+ *
+ * @export
+ * @interface ILink
+ */
 export interface ILink {
+  /**
+   * A unique identifier for the link
+   * Generated using nanoid
+   */
   _id: string
+
+  /**
+   * The link's from side's Pin's ID
+   */
   from: string
+
+  /**
+   * The link's to side's Pin's ID
+   */
   to: string
+
+  /**
+   * The link's type
+   */
   type: string
+
+  /**
+   * The link's metadata
+   */
   metadata: unknown
 }
 
--- apps/server/src/db/pin.ts
+++ apps/server/src/db/pin.ts
@@ -1,9 +1,25 @@
 import { database } from './base.js'
 
 export interface IPin {
+  /**
+   * The Pin's ID
+   * We'll use the DOID
+   */
   _id: string
+
+  /**
+   * The Pin's type
+   */
   type: string
+
+  /**
+   * The Pin's owner, for Access Control
+   */
   owner: string
+
+  /**
+   * The Pin's metadata
+   */
   metadata: unknown
 }
 
