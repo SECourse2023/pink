828afb41dd3697aedecc0b281435b130572cc504
忘忧北萱草 <wybxc@qq.com>
2023-04-17 11:45:34 +0000

    feat: add layout components

--- apps/ui/pages/index.tsx
+++ apps/ui/pages/index.tsx
@@ -1,10 +1,5 @@
 import { Flex, Heading, Spacer } from '@chakra-ui/react'
 
 export default function Index() {
-  return (
-    <Flex px={4} py={2}>
-      <Heading>Pink</Heading>
-      <Spacer />
-    </Flex>
-  )
+  return <Heading>TODO</Heading>
 }
