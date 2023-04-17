c47697b3a4a1d617d1e27dbab70b1c0e696e8f35
忘忧北萱草 <wybxc@qq.com>
2023-04-17 12:09:43 +0000

    feat: add login page UI

--- apps/ui/pages/login.tsx
+++ apps/ui/pages/login.tsx
@@ -1,7 +1,28 @@
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import React from 'react'
+import { useRouter } from 'next/router'
 export default function Login() {
+  const router = useRouter()
+  const onLogin = () => {
+    alert('登录成功')
+    router.push('/')
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Box maxW="md" mx="auto" mt={8} borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Box p="6">
+        <FormControl>
+          <FormLabel>用户名</FormLabel>
+          <Input placeholder="输入用户名" />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>密码</FormLabel>
+          <Input type="password" placeholder="输入密码" />
+        </FormControl>
+        <Button colorScheme="pink" mt={8} onClick={onLogin} w="full">
+          登录
+        </Button>
+      </Box>
+    </Box>
   )
 }
