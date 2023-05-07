adc79a58e78f4f43f834e1645b1dd897e2ca7ef5
Zisu Zhang <thezzisu@gmail.com>
2023-05-07 20:55:53 +0800

    feat: add register doid (#16)
    * feat: implement register doid
    
    * feat: integrate with gateway

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -8,3 +8,4 @@ function env(key: string, defaultValue: string): string {
 
 export const MONGO_URI = env('MONGO_URI', 'mongodb://db:27017/pink')
 export const JWT_SECRET = env('JWT_SECRET', 'topSECRET')
+export const GATEWAY_URL = env('GATEWAY_URL', 'http://gateway:8000')
