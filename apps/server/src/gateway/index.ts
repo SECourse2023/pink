4cab47b7e1a929aef9ed0da881388e9ee8aa3705
thezzisu <thezzisu@gmail.com>
2023-05-07 12:54:48 +0000

    feat: integrate with gateway

--- apps/server/src/gateway/index.ts
+++ apps/server/src/gateway/index.ts
@@ -1,5 +1,13 @@
-import { nanoid } from 'nanoid/async'
+import { GATEWAY_URL } from '../config/index.js'
 
 export async function registerDOID(metadata: Record<string, unknown>) {
-  return nanoid()
+  const resp = await fetch(`${GATEWAY_URL}/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(metadata)
+  })
+  const data = await resp.json()
+  return data as string
 }
