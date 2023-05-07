3c45b7887d6c229687087b00259d1c2deb0425c5
thezzisu <thezzisu@gmail.com>
2023-05-07 17:24:32 +0000

    feat: update dashboard

--- apps/ui/pages/dashboard.tsx
+++ apps/ui/pages/dashboard.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import PinListView from '../components/PinListView'
-import pinsData from '../data/pinsData'
+import { http } from '../utils/ky'
 
 const DashBoardView: React.FC = () => {
+  const [pins, setPins] = useState<any[]>([])
+  useEffect(() => {
+    http
+      .get('/api/pin/list')
+      .json()
+      .then((data) => setPins(data as any))
+  })
   return (
     <Flex>
       <Box width="2/7" backgroundColor="white">
         <Heading as="h1" size="lg" textAlign="center" py={5}>
           Pink
         </Heading>
-        <PinListView pins={pinsData} />
+        <PinListView pins={pins} />
       </Box>
       <Box width="5/7" backgroundColor="gray.100">
         {/* Right side content */}
