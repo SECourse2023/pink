0bf6896a86bfc716dccb727893f1567a2cb9130e
py <py@email.com>
2023-05-18 13:55:17 +0000

    query backend

--- apps/ui/pages/query.tsx
+++ apps/ui/pages/query.tsx
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react'
-import { Flex, Box, Input, Button, useToast, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Input,
+  Button,
+  useToast,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter
+} from '@chakra-ui/react'
 import { http } from '../utils/ky'
 
 const QueryView: React.FC = () => {
@@ -25,25 +38,27 @@ const QueryView: React.FC = () => {
       }
 
       try {
-        const data = await http.post('/api/query/getLinkOf', {
-          json: {
-            from: fromDoid,
-            to: toDoid
-          }
-        }).json()
+        const data = await http
+          .post('/api/query/getLinkOf', {
+            json: {
+              from: fromDoid,
+              to: toDoid
+            }
+          })
+          .json()
         setResult(data)
         toast({
-          title: "Request successful.",
+          title: 'Request successful.',
           description: "We've sent your request to the server.",
-          status: "success",
+          status: 'success',
           duration: 5000,
-          isClosable: true,
+          isClosable: true
         })
         setIsOpen(true)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-      
+
       setShouldSubmit(false)
     }
 
@@ -58,61 +73,59 @@ const QueryView: React.FC = () => {
       height="100vh"
       bg="gray.100"
     >
-      {<Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-      bg="gray.100"
-    >
-      <Box
-        w="100%"
-        maxWidth="500px"
-        p="2rem"
-        boxShadow="0 2px 8px rgba(0, 0, 0, 0.1)"
-        borderRadius="8px"
-        bg="white"
-      >
-        <Box as="form" w="100%">
-          <Input
-            mb="1rem"
-            type="text"
-            value={fromDoid}
-            onChange={(e) => setFromDoid(e.target.value)}
-            placeholder="From DOID"
-            variant="outline"
-          />
-          <Input
-            mb="1rem"
-            type="text"
-            value={toDoid}
-            onChange={(e) => setToDoid(e.target.value)}
-            placeholder="To DOID"
-            variant="outline"
-          />
-          <Button
-            onClick={handleSubmit}
-            bg="#2196f3"
-            color="white"
-            fontWeight="bold"
-            borderRadius="4px"
+      {
+        <Flex
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100vh"
+          bg="gray.100"
+        >
+          <Box
+            w="100%"
+            maxWidth="500px"
+            p="2rem"
+            boxShadow="0 2px 8px rgba(0, 0, 0, 0.1)"
+            borderRadius="8px"
+            bg="white"
           >
-            Submit
-          </Button>
-          {result && (
-            <pre>{JSON.stringify(result, null, 2)}</pre>
-          )}
-        </Box>
-      </Box>
-    </Flex>}
+            <Box as="form" w="100%">
+              <Input
+                mb="1rem"
+                type="text"
+                value={fromDoid}
+                onChange={(e) => setFromDoid(e.target.value)}
+                placeholder="From DOID"
+                variant="outline"
+              />
+              <Input
+                mb="1rem"
+                type="text"
+                value={toDoid}
+                onChange={(e) => setToDoid(e.target.value)}
+                placeholder="To DOID"
+                variant="outline"
+              />
+              <Button
+                onClick={handleSubmit}
+                bg="#2196f3"
+                color="white"
+                fontWeight="bold"
+                borderRadius="4px"
+              >
+                Submit
+              </Button>
+              {result && <pre>{JSON.stringify(result, null, 2)}</pre>}
+            </Box>
+          </Box>
+        </Flex>
+      }
       <Modal isOpen={isOpen} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Result</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            {JSON.stringify(result)}
-          </ModalBody>
+          <ModalBody>{JSON.stringify(result)}</ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" onClick={handleClose}>
               Close
