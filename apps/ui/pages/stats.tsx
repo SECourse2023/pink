b42d93696d687274cbb1bc4f6a62c10ca91d9123
忘忧北萱草 <wybxc@qq.com>
2023-05-29 19:42:39 +0800

    feat: better stats page (#41)

--- apps/ui/pages/stats.tsx
+++ apps/ui/pages/stats.tsx
@@ -1,21 +1,21 @@
 import {
   Flex,
-  Avatar,
   Text,
   Wrap,
   WrapItem,
   Heading,
   Center,
-  Spacer,
-  Button
+  Card,
+  CardHeader,
+  CardBody,
+  VStack
 } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import { Stack, HStack, VStack } from '@chakra-ui/react'
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { http } from '../utils/ky'
-import md5 from 'md5'
-import { useRouter } from 'next/router'
-import { AuthContext } from '../contexts/auth'
+import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement } from 'chart.js'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement)
 
 function StatsView() {
   const [stats, setStats] = useState<any>({})
@@ -26,17 +26,76 @@ function StatsView() {
       .json()
       .then((data) => setStats(data))
   }, [])
+
+  const data = {
+    labels: ['Pins', 'Links', 'Users'],
+    datasets: [
+      {
+        label: 'Statistics',
+        data: [stats.pins, stats.links, stats.users],
+        backgroundColor: ['#8884d8', '#82ca9d', '#ffc658'],
+        borderWidth: 1
+      }
+    ]
+  }
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          stepSize: 1,
+          precision: 0
+        }
+      }
+    }
+  }
+
   return (
-    <VStack>
-      <Heading as="div" size="xl" mx="10" my="10">
-        pin数量{stats.pins}
-      </Heading>
-      <Heading as="div" size="xl" mx="10" my="10">
-        links数量{stats.links}
-      </Heading>
-      <Heading as="div" size="xl" mx="10" my="10">
-        user数量{stats.users}
-      </Heading>
+    <VStack spacing={4} align="center">
+      <Card>
+        <CardHeader>
+          <Heading as="h2" size="md">
+            统计数据
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          <Center mb={4}>
+            <Wrap gap={8}>
+              <WrapItem>
+                <Flex direction="row" align="baseline">
+                  <Text fontSize="2xl" fontWeight="bold">
+                    {stats.pins}
+                  </Text>
+                  <Text fontSize="sm" color="gray.500" textAlign="center">
+                    Pins
+                  </Text>
+                </Flex>
+              </WrapItem>
+              <WrapItem>
+                <Flex direction="row" align="baseline">
+                  <Text fontSize="2xl" fontWeight="bold">
+                    {stats.links}
+                  </Text>
+                  <Text fontSize="sm" color="gray.500" textAlign="center">
+                    Links
+                  </Text>
+                </Flex>
+              </WrapItem>
+              <WrapItem>
+                <Flex direction="row" align="baseline">
+                  <Text fontSize="2xl" fontWeight="bold">
+                    {stats.users}
+                  </Text>
+                  <Text fontSize="sm" color="gray.500" textAlign="center">
+                    Users
+                  </Text>
+                </Flex>
+              </WrapItem>
+            </Wrap>
+          </Center>
+          <Bar data={data} options={options} />
+        </CardBody>
+      </Card>
     </VStack>
   )
 }
